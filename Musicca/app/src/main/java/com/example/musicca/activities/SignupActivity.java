f7289076e51f2686119b0fd218a88930d0937a0d
Grace Wainaina <7gracewainaina@gmail.com>
2020-07-14 05:22:47 -0700

    Sign up
    User is able to sign up to the up

---
+++ Musicca/app/src/main/java/com/example/musicca/activities/SignupActivity.java
@@ -0,0 +1,73 @@
+package com.example.musicca.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.musicca.R;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignupActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignupActivity";
+    private EditText etNewusername;
+    private EditText etNewpassword;
+    private Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
+        etNewpassword = findViewById(R.id.etNewpassword);
+        etNewusername = findViewById(R.id.etNewusername);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick sign up button");
+                // Create the ParseUser
+                ParseUser user = new ParseUser();
+                Log.d(TAG, "user created 1");
+                signupUser(user);
+                Log.d(TAG, "user created 2");
+            }
+        });
+    }
+
+    private void signupUser(ParseUser parseUser) {
+        // Set core properties
+        parseUser.setUsername(etNewusername.getText().toString());
+        parseUser.setPassword(etNewpassword.getText().toString());
+        Log.d(TAG, "user properties stored");
+        // Invoke signUpInBackground
+
+        parseUser.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "issue with sign up", e);
+                    Toast.makeText(SignupActivity.this, "Issue with signup!", Toast.LENGTH_SHORT).show();
+                }
+                goMainActivity();
+                Toast.makeText(SignupActivity.this, "Sign up Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        Log.d(TAG, "going to main activity");
+        finish();
+    }
+}
---
+++ Musicca/app/src/main/java/com/example/musicca/models/ParseApplication.java
@@ -0,0 +1,42 @@
+package com.example.musicca.models;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // Register your parse models
+        //ParseUser.registerSubclass(User.class);
+        //ParseObject.registerSubclass(Comment.class);
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("grace-musicca") // should correspond to APP_ID env variable
+                .clientKey("grace-musiccaMasterKeyParse")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("https://grace-musicca.herokuapp.com/parse").build());
+    }
+}
