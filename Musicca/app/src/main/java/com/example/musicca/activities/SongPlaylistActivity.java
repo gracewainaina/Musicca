36c00c5e7bc4a7dce39481975b1f563d997d418d
Grace Wainaina <7gracewainaina@gmail.com>
2020-08-04 13:42:05 -0700

    set up play previous and play next

--- Musicca/app/src/main/java/com/example/musicca/activities/SongPlaylistActivity.java
+++ Musicca/app/src/main/java/com/example/musicca/activities/SongPlaylistActivity.java
@@ -3,6 +3,7 @@ package com.example.musicca.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityOptionsCompat;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -29,6 +30,8 @@ import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
+import java.util.List;
+
 public class SongPlaylistActivity extends AppCompatActivity {
 
     private static final String EXTRA_PLAYLISTOBJECTID = "playlistobjectid";
@@ -39,6 +42,10 @@ public class SongPlaylistActivity extends AppCompatActivity {
 
 
     private static final String TAG = "Play Song";
+
+    private List<String> sortedSongObjectIds;
+    private int songPosition;
+    private String songSpotifyId;
 
     private ImageView ivAlbum;
     private TextView tvTitle;
@@ -78,20 +85,53 @@ public class SongPlaylistActivity extends AppCompatActivity {
         ivPlayPause = findViewById(R.id.ivPlayPause);
         ivNext = findViewById(R.id.ivNext);
 
-        songObjectId = getIntent().getStringExtra(EXTRA_SONGOBJECTID);
-        playlistObjectId = getIntent().getStringExtra(EXTRA_PLAYLISTOBJECTID);
+        sortedSongObjectIds = getIntent().getStringArrayListExtra("songList");
+        songPosition = getIntent().getIntExtra("currentSongPosition", 0);
 
-        albumUrl = getIntent().getStringExtra(EXTRA_ALBUMICONURL);
-        Glide.with(this).load(albumUrl).into(ivAlbum);
+        setSongView();
 
-        tvTitle.setText(getIntent().getStringExtra(EXTRA_SONGTITLE));
-        tvArtist.setText(getIntent().getStringExtra(EXTRA_SONGARTIST));
+//        songObjectId = getIntent().getStringExtra(EXTRA_SONGOBJECTID);
+//        playlistObjectId = getIntent().getStringExtra(EXTRA_PLAYLISTOBJECTID);
+//
+//        albumUrl = getIntent().getStringExtra(EXTRA_ALBUMICONURL);
+//        Glide.with(this).load(albumUrl).into(ivAlbum);
+//
+//        tvTitle.setText(getIntent().getStringExtra(EXTRA_SONGTITLE));
+//        tvArtist.setText(getIntent().getStringExtra(EXTRA_SONGARTIST));
 
         ivPlayPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                playSong(songObjectId);
+                playSong();
                 ivPlayPause.setImageResource(R.drawable.pauseicon);
+            }
+        });
+
+        ivPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (songPosition == 0) {
+                    Toast toast= Toast.makeText(SongPlaylistActivity.this, "This is the first song!", Toast.LENGTH_SHORT);
+                    ivPrevious.setVisibility(View.GONE);
+                }
+                else{
+                    songPosition--;
+                    setSongView();
+                }
+            }
+        });
+
+        ivNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (songPosition == sortedSongObjectIds.size()-1) {
+                    Toast toast= Toast.makeText(SongPlaylistActivity.this, "This is the last song!", Toast.LENGTH_SHORT);
+                    ivNext.setVisibility(View.GONE);
+                }
+                else{
+                    songPosition++;
+                    setSongView();
+                }
             }
         });
 
@@ -103,45 +143,73 @@ public class SongPlaylistActivity extends AppCompatActivity {
         });
     }
 
+    private void setSongView() {
+        if (sortedSongObjectIds != null && sortedSongObjectIds.size() > songPosition) {
+            String songObjectId = sortedSongObjectIds.get(songPosition);
+
+            ParseQuery<Song> querySong = ParseQuery.getQuery(Song.class);
+            querySong.getInBackground(songObjectId, new GetCallback<Song>() {
+                @Override
+                public void done(Song song, com.parse.ParseException e) {
+                    if (e == null) {
+                        Glide.with(SongPlaylistActivity.this).load(song.getURL()).into(ivAlbum);
+                        tvTitle.setText(song.getTitle());
+                        tvArtist.setText(song.getArtist());
+                        songSpotifyId = song.getSpotifyId();
+
+                    } else {
+                        Toast.makeText(SongPlaylistActivity.this, "Error showing song!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
     private void gotoPlaylist() {
         Intent i = new Intent(this, CurrentPlaylistActivity.class);
         i.putExtra(EXTRA_PLAYLISTOBJECTID, playlistObjectId);
         startActivity(i);
     }
 
-    private void playSong(String songObjectId) {
-        ParseQuery<Song> query = ParseQuery.getQuery(Song.class);
-        // Execute the query to find the object with ID
-        query.getInBackground(songObjectId, new GetCallback<Song>() {
-            @Override
-            public void done(Song song, com.parse.ParseException e) {
-                if (e == null) {
-                    String spotifyID = song.getSpotifyId();
+    private void playSong() {
 
-                    ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID).setRedirectUri(REDIRECT_URI).showAuthView(true).build();
-                    SpotifyAppRemote.connect(SongPlaylistActivity.this, connectionParams,
-                            new Connector.ConnectionListener() {
+        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID).setRedirectUri(REDIRECT_URI).showAuthView(true).build();
+        SpotifyAppRemote.connect(SongPlaylistActivity.this, connectionParams,
+                new Connector.ConnectionListener() {
 
-                                @Override
-                                public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                                    mSpotifyAppRemote = spotifyAppRemote;
-                                    Log.d(TAG, "Connected!");
-                                    // Now you can start interacting with App Remote
-                                    mSpotifyAppRemote.getPlayerApi().play("spotify:track:" + spotifyID);
-                                }
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d(TAG, "Connected!");
+                        // Now you can start interacting with App Remote
+                        mSpotifyAppRemote.getPlayerApi().play("spotify:track:" + songSpotifyId);
+                    }
 
-                                @Override
-                                public void onFailure(Throwable throwable) {
-                                    Log.e(TAG, throwable.getMessage(), throwable);
-                                    Toast.makeText(SongPlaylistActivity.this, "Error playing song!", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                } else {
-                    Log.d(TAG, "play song not found!");
-                    Toast.makeText(SongPlaylistActivity.this, "Error playing song!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e(TAG, throwable.getMessage(), throwable);
+                        Toast.makeText(SongPlaylistActivity.this, "Error playing song!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+//        ParseQuery<Song> query = ParseQuery.getQuery(Song.class);
+//        // Execute the query to find the object with ID
+//        query.getInBackground(songObjectId, new GetCallback<Song>() {
+//            @Override
+//            public void done(Song song, com.parse.ParseException e) {
+//                if (e == null) {
+//                    String spotifyID = song.getSpotifyId();
+//
+//
+//                } else {
+//                    Log.d(TAG, "play song not found!");
+//                    Toast.makeText(SongPlaylistActivity.this, "Error playing song!", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
     }
 
     @Override
